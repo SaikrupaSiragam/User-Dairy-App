1d68f7a7e883ff973255d5788772528c63bc7ea2
SaikrupaSiragam <86454447+SaikrupaSiragam@users.noreply.github.com>
2022-06-06 14:57:00 +0530

    Add files via upload

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginComponent from './Login';
+
+const Home = () => {
+  
+  return (
+    <div className='home'>
+        <h1>Dairy Application</h1>
+        <LoginComponent/>
+    </div>
+  );
+}
+
+export default Home;
